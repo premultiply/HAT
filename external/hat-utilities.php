15e98817dc41a54cafaab1ee27c7fea353f260dc
premultiply <ulrich.peters@posteo.de>
2016-05-19 23:08:25 +0200

    Update hat-utilities.php

--- external/hat-utilities.php
+++ external/hat-utilities.php
@@ -1,59 +1,52 @@
-
 <?php
- /*
-
- Theme Name:    hat
-Theme URI:      http://hat.fokus.fraunhofer.de/wordpress/
-Description:    HbbTV Application Toolkit
-Version:        0.1
-Author:         Fraunhofer Fokus
-Author URI:     http://www.fokus.fraunhofer.de/go/fame
-Tags:           hbbtv
-
+/*
+Theme Name: 	hat
+Theme URI: 		http://hat.fokus.fraunhofer.de/wordpress/
+Description: 	HbbTV Application Toolkit
+Version: 		0.1
+Author: 		Fraunhofer Fokus
+Author URI: 	http://www.fokus.fraunhofer.de/go/fame
+Tags: 			hbbtv
 */
-	 class Hat_Utilities {
 
-    	public static function print_a( $a ) {
-    		print( '<pre>' );
-    		print_r( $a );
-    		print( '</pre>' );
-    	}
+class Hat_Utilities {
+	public static function print_a( $a ) {
+		print( '<pre>' );
+		print_r( $a );
+		print( '</pre>' );
+	}
 
+	public static function get_template_parts( $parts = array() ) {
+		foreach( $parts as $part ) {
+			get_template_part( $part );
+		};
+	}
 
-    	public static function get_template_parts( $parts = array() ) {
-    		foreach( $parts as $part ) {
-    			get_template_part( $part );
-    		};
-    	}
+	public static function get_page_id_from_path( $path ) {
+		$page = get_page_by_path( $path );
+		if( $page ) {
+			return $page->ID;
+		} else {
+			return null;
+		};
+	}
 
-    	public static function get_page_id_from_path( $path ) {
-    		$page = get_page_by_path( $path );
-    		if( $page ) {
-    			return $page->ID;
-    		} else {
-    			return null;
-    		};
-    	}
+	public static function add_slug_to_body_class( $classes ) {
+		global $post;
+		if( is_page() ) {
+			$classes[] = sanitize_html_class( $post->post_name );
+		} elseif(is_singular()) {
+			$classes[] = sanitize_html_class( $post->post_name );
+		};
+		return $classes;
+	}
 
-    	public static function add_slug_to_body_class( $classes ) {
-    		global $post;
-	   
-    		if( is_page() ) {
-    			$classes[] = sanitize_html_class( $post->post_name );
-    		} elseif(is_singular()) {
-    			$classes[] = sanitize_html_class( $post->post_name );
-    		};
+	public static function get_category_id( $cat_name ){
+		$term = get_term_by( 'name', $cat_name, 'category' );
+		return $term->term_id;
+	}
 
-    		return $classes;
-    	}
-	
-    	public static function get_category_id( $cat_name ){
-    		$term = get_term_by( 'name', $cat_name, 'category' );
-    		return $term->term_id;
-    	}
-
-        public static function minified(){
-            return '';
-        }
-	
-    }
+	public static function minified(){
+		return '';
+	}
+}
